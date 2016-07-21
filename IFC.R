065fcc1fcd544ad1ce93048aed208ccb7a90eed9
tylerjrichards <tylerjrichards@gmail.com>
2016-07-21 10:09:50 -0400

    Update IFC.R

--- IFC.R
+++ IFC.R
@@ -1,1 +1,18 @@
+#this analysis is much easier than the others. The summary function gives a good indication of the breakdown, especially compared to the racial breakdown
+#load dataset
+admin <- `BK.Entrance...Sheet1.(1)`
 
+
+#breakdown of IFC
+IFC <- subset(admin$IFC.Fraternity != 'None')
+summary(IFC)
+barplot(IFC)
+
+#compared to total
+#can be easily accomplished from summary(admin)
+summary(admin)
+
+#or instead of ^
+
+v <- admin$Race
+summary(v)
